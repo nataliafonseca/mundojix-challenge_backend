040f7403c621131ae0736fbcf12bb1afd0b7a236
Natália Fonseca <nataliabf@outlook.com>
2021-10-23 11:31:05 -0300

    chore: add ts-jest to fix jest not working with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverageFrom: ['src/useCases/**/*.ts'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 };
