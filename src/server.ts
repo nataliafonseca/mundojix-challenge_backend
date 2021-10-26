4eddb793ff3d1176873f677ffb3dcd27b3b3dbc4
Natália Fonseca <nataliabf@outlook.com>
2021-10-25 22:14:00 -0300

    feat: add documentation

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
-import { router } from './routes';
-import dotenv from 'dotenv';
-import { handleErrors } from './middleware/handleErrors';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { handleErrors } from './middleware/handleErrors';
+import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 dotenv.config();
 
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(cors({ origin: '*', exposedHeaders: ['X-Total-Count'] }));
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(handleErrors);
